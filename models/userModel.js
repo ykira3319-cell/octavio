9425e9193d969cc5e048dc3c30db10814b5d2e75
ykira3319-cell <ykira3319@gmail.com>
2026-03-18 01:01:50 +0100

    Create userModel.js

---
+++ models/userModel.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  favorites: [String],
+  history: [String],
+});
+
+export default mongoose.model("User", UserSchema);
